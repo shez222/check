cda2d6f9f23baf05248323371c5606a5e189fd49
shez222 <bilal.shehroz420@gmail.com>
2024-08-18 07:58:07 +0500

    changevv

--- firebase.ts
+++ firebase.ts
@@ -3,24 +3,24 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAkv0YO_7BseiY5xb9Q_U5WKut_lB4LbGc",
-  authDomain: "careerspacesapp.firebaseapp.com",
-  projectId: "careerspacesapp",
-  storageBucket: "careerspacesapp.appspot.com",
-  messagingSenderId: "1023642701634",
-  appId: "1:221445168616:web:627d0533d0f77ae4dbd85d",
-  measurementId: "G-6HRWE0D310"
-};
 // const firebaseConfig = {
-//   apiKey: "AIzaSyAr-yQEsSPLMdinQ8GHITRYFUbiTwd8KAc",
-//   authDomain: "school-web-c3ed5.firebaseapp.com",
-//   projectId: "school-web-c3ed5",
-//   storageBucket: "school-web-c3ed5.appspot.com",
-//   messagingSenderId: "1066637057144",
-//   appId: "1:1066637057144:web:504912fab65a5f2333e18d",
-//   measurementId: "G-H26P0TE4N0"
+//   apiKey: "AIzaSyAkv0YO_7BseiY5xb9Q_U5WKut_lB4LbGc",
+//   authDomain: "careerspacesapp.firebaseapp.com",
+//   projectId: "careerspacesapp",
+//   storageBucket: "careerspacesapp.appspot.com",
+//   messagingSenderId: "1023642701634",
+//   appId: "1:221445168616:web:627d0533d0f77ae4dbd85d",
+//   measurementId: "G-6HRWE0D310"
 // };
+const firebaseConfig = {
+  apiKey: "AIzaSyAr-yQEsSPLMdinQ8GHITRYFUbiTwd8KAc",
+  authDomain: "school-web-c3ed5.firebaseapp.com",
+  projectId: "school-web-c3ed5",
+  storageBucket: "school-web-c3ed5.appspot.com",
+  messagingSenderId: "1066637057144",
+  appId: "1:1066637057144:web:504912fab65a5f2333e18d",
+  measurementId: "G-H26P0TE4N0"
+};
 // Initialize Firebase
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
