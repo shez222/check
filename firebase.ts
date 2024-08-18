ab4c4158af6467c083f77d346adf35b07d615b4c
shez222 <bilal.shehroz420@gmail.com>
2024-08-18 09:11:24 +0500

    change iconsjh

--- firebase.ts
+++ firebase.ts
@@ -12,24 +12,24 @@ import { getAuth } from "firebase/auth";
 //   appId: "1:221445168616:web:627d0533d0f77ae4dbd85d",
 //   measurementId: "G-6HRWE0D310"
 // };
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBC-icTCglwnJGWbAiy9FrN4XEI76xpSy0",
-//   authDomain: "fir-auth-58ae9.firebaseapp.com",
-//   projectId: "fir-auth-58ae9",
-//   storageBucket: "fir-auth-58ae9.appspot.com",
-//   messagingSenderId: "409486673502",
-//   appId: "1:409486673502:web:dd432c7570c80d5d2196cf",
-//   measurementId: "G-N5WV300PKS"
-//  };
 const firebaseConfig = {
-  apiKey: "AIzaSyAr-yQEsSPLMdinQ8GHITRYFUbiTwd8KAc",
-  authDomain: "school-web-c3ed5.firebaseapp.com",
-  projectId: "school-web-c3ed5",
-  storageBucket: "school-web-c3ed5.appspot.com",
-  messagingSenderId: "1066637057144",
-  appId: "1:1066637057144:web:504912fab65a5f2333e18d",
-  measurementId: "G-H26P0TE4N0"
+  apiKey: "AIzaSyC1SPNqM9kB708ZwtxLhlagSfIg82vLDUU",
+  authDomain: "test-3aa9d.firebaseapp.com",
+  projectId: "test-3aa9d",
+  storageBucket: "test-3aa9d.appspot.com",
+  messagingSenderId: "111464666312",
+  appId: "1:111464666312:web:fb0801c08699ccfebd41fd",
+  measurementId: "G-33F68Q2RD6"
 };
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAr-yQEsSPLMdinQ8GHITRYFUbiTwd8KAc",
+//   authDomain: "school-web-c3ed5.firebaseapp.com",
+//   projectId: "school-web-c3ed5",
+//   storageBucket: "school-web-c3ed5.appspot.com",
+//   messagingSenderId: "1066637057144",
+//   appId: "1:1066637057144:web:504912fab65a5f2333e18d",
+//   measurementId: "G-H26P0TE4N0"
+// };
 // Initialize Firebase
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
